b8942c478fb66f3b076fdf44dffc5615f186f81a
Joe Crop <joe.crop@gmail.com>
2023-12-06 13:58:41 -0800

    Updates

--- tv-frame/index.js
+++ tv-frame/index.js
@@ -3,6 +3,14 @@ setTimeout(function() {
     window.location.reload();
 }, 1000 * 60 * 60 * 24);
     
+async function getImageDimensions(src){
+  return new Promise((resolve, reject) => {
+    let img = new Image()
+    img.onload = () => resolve({height: img.height, width: img.width})
+    img.onerror = reject
+    img.src = src
+  })
+}
 
 
 // Function to change the image every 10 minutes
@@ -14,30 +22,57 @@ async function changeImage() {
     let randomNumber = Math.floor(Math.random() * 100) + 1;
 
     try {
-        await new Promise((resolve, reject) => {
+      await new Promise((resolve, reject) => {
         const img = new Image();
         img.onload = resolve;
         img.onerror = reject;
-        img.src = `matted/${randomNumber}.png`;
+        img.src = `originals/${randomNumber}.png`;
       });
-      path = `matted/${randomNumber}.png`;
+      path = `originals/${randomNumber}.png`;
     } catch (error) {
       try {
         await new Promise((resolve, reject) => {
           const img = new Image();
           img.onload = resolve;
           img.onerror = reject;
-          img.src = `matted/${randomNumber}.jpg`;
+          img.src = `originals/${randomNumber}.jpg`;
         });
-        path = `matted/${randomNumber}.jpg`;
+        path = `originals/${randomNumber}.jpg`;
       } catch (error) {
         path = "";
       }
     }
   }
-  
+
+  // calculate dimensions
+  const dimensions = await getImageDimensions(path);
+
+  const totalHeight = window.innerHeight;
+  const totalWidth = window.innerWidth;
+  let artWidth = 0;
+  let artHeight = 0;
+  if(dimensions.height/totalHeight > dimensions.width/totalWidth) {
+    // shrink height and scale width accordingly
+    artHeight = totalHeight * 0.8;
+    const scale = artHeight / dimensions.height;
+    artWidth = dimensions.width * scale;
+  } else {
+    // shrink width and scale height accordingly
+    artWidth = totalWidth * 0.8;
+    const scale = artWidth / dimensions.width;
+    artHeight = dimensions.height * scale; 
+  }
+
   // Set the current image
-  document.body.style.backgroundImage = `url('${path}')`;
+  document.getElementById("art").src = path;
+  // Set the width and height of the image
+  console.log(dimensions)
+  document.getElementById("art").style.width = artWidth + "px";
+  document.getElementById("art").style.height = artHeight + "px";
+  document.getElementById("content").style.width = artWidth + "px";
+  document.getElementById("content").style.height = artHeight + "px";
+  document.getElementById("content").style.display = "block";
+  
 }
 
 // Get a new image
